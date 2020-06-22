229184511c7a74b64d541992bdd98592a4fec4c7
rajdeep1008 <iamrajdeep1008@gmail.com>
2020-06-23 02:46:28 +0530

    Passed week 1 assignment

--- week1/assignment/code/assignment.cpp
+++ week1/assignment/code/assignment.cpp
@@ -1,23 +1,42 @@
+#include <iostream>
 #include <opencv2/opencv.hpp>
-#include <vector>
+#include <string>
 
 using namespace std;
 using namespace cv;
 
+void mark_qr_box(Mat &im, Mat &b_box) {
+    int n = b_box.rows;
+    for (int i = 0; i < n; i++) {
+        line(im, Point2i(b_box.at<Point2f>(i)),
+             Point2i(b_box.at<Point2f>((i + 1) % n)), Scalar(255, 0, 0), 2);
+    }
+}
+
 int main(void) {
     string image_path = "./res/IDCard-Satya.png";
-    Mat img = imread(image_path, IMREAD_UNCHANGED);
+    Mat img = imread(image_path, IMREAD_COLOR);
 
-    cout << "Size: " << img.size << ", Channels: " << img.channels() << endl;
+    cout << "Size: " << img.size() << ", Channels: " << img.channels() << endl;
+
+    Mat bbox, rectifiedImage;
 
     QRCodeDetector qrDecoder = QRCodeDetector();
-    OutputArray opencvData = noArray();
+    string opencvData;
+    opencvData = qrDecoder.detectAndDecode(img, bbox, rectifiedImage);
 
-    cout << opencvData.getMat() << endl;
+    if (opencvData.length() > 0) {
+        cout << "QR Code Detected" << endl;
+        cout << opencvData << endl;
+    } else {
+        cout << "QR Code NOT Detected" << endl;
+        return 0;
+    }
 
-    bool hasQrCode = qrDecoder.detect(img, opencvData);
+    mark_qr_box(img, bbox);
 
-    cout << opencvData.getMat() << " : " << hasQrCode;
+    string result_image_path = "./res/QRCode-Output.png";
+    imwrite(result_image_path, img);
 
     return 0;
 }
